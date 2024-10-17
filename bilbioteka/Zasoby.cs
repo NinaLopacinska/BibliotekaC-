9c8a9af814948375912e70394b8153f079e66aa9
Nina Łopacińska <UL0273299@edu.uni.lodz.pl>
2024-10-17 10:53:36 +0200

    Klasa zasoby

---
+++ bilbioteka/Zasoby.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilbioteka
+{
+    internal class Zasoby
+    {
+        public Zasoby(string tytul, string autor, int rokWydania, int numerKatalogowy, string typ, bool czyWypozyczone)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            RokWydania = rokWydania;
+            NumerKatalogowy = numerKatalogowy;
+            Typ = typ;
+            CzyWypozyczone = czyWypozyczone;
+        }
+
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public int RokWydania { get; set; }
+        public int NumerKatalogowy { get; set; }
+        public string Typ { get; set; }
+        public bool CzyWypozyczone { get; set; }
+    }
+}
