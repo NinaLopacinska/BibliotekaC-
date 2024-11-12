c6865c0fc6290fe18bf8fb933a6f62910506060f
Wiktor Kozak <wikitero15@gmail.com>
2024-11-12 11:37:00 +0100

    zmiana rozciągania tła Dodajpracownikaform

--- bilbioteka/Forms/DodajPracownikaForm.Designer.cs
+++ bilbioteka/Forms/DodajPracownikaForm.Designer.cs
@@ -67,9 +67,10 @@
             // buttonRejstracja
             // 
             buttonRejstracja.Font = new Font("Book Antiqua", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            buttonRejstracja.Location = new Point(436, 828);
+            buttonRejstracja.Location = new Point(382, 621);
+            buttonRejstracja.Margin = new Padding(3, 2, 3, 2);
             buttonRejstracja.Name = "buttonRejstracja";
-            buttonRejstracja.Size = new Size(169, 62);
+            buttonRejstracja.Size = new Size(148, 46);
             buttonRejstracja.TabIndex = 75;
             buttonRejstracja.Text = "Rejestruj";
             buttonRejstracja.UseVisualStyleBackColor = true;
@@ -77,9 +78,10 @@
             // buttonZalogujRej
             // 
             buttonZalogujRej.Font = new Font("Book Antiqua", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonZalogujRej.Location = new Point(12, 12);
+            buttonZalogujRej.Location = new Point(10, 9);
+            buttonZalogujRej.Margin = new Padding(3, 2, 3, 2);
             buttonZalogujRej.Name = "buttonZalogujRej";
-            buttonZalogujRej.Size = new Size(88, 69);
+            buttonZalogujRej.Size = new Size(77, 52);
             buttonZalogujRej.TabIndex = 41;
             buttonZalogujRej.Text = " ⬅";
             buttonZalogujRej.UseVisualStyleBackColor = true;
@@ -88,17 +90,19 @@
             // dateTimePicker1
             // 
             dateTimePicker1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            dateTimePicker1.Location = new Point(190, 430);
+            dateTimePicker1.Location = new Point(166, 322);
+            dateTimePicker1.Margin = new Padding(3, 2, 3, 2);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(267, 27);
+            dateTimePicker1.Size = new Size(234, 23);
             dateTimePicker1.TabIndex = 113;
             // 
             // buttonDodajProdukt
             // 
             buttonDodajProdukt.Font = new Font("Book Antiqua", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            buttonDodajProdukt.Location = new Point(190, 610);
+            buttonDodajProdukt.Location = new Point(166, 458);
+            buttonDodajProdukt.Margin = new Padding(3, 2, 3, 2);
             buttonDodajProdukt.Name = "buttonDodajProdukt";
-            buttonDodajProdukt.Size = new Size(169, 62);
+            buttonDodajProdukt.Size = new Size(148, 46);
             buttonDodajProdukt.TabIndex = 112;
             buttonDodajProdukt.Text = "Dodaj";
             buttonDodajProdukt.UseVisualStyleBackColor = true;
@@ -108,99 +112,109 @@
             // 
             label20.AutoSize = true;
             label20.Font = new Font("Book Antiqua", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label20.Location = new Point(526, 283);
+            label20.Location = new Point(460, 212);
             label20.Name = "label20";
-            label20.Size = new Size(101, 22);
+            label20.Size = new Size(82, 18);
             label20.TabIndex = 111;
             label20.Text = "Opcjonalnie";
             // 
             // textBoxEmail
             // 
             textBoxEmail.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxEmail.Location = new Point(190, 480);
+            textBoxEmail.Location = new Point(166, 360);
+            textBoxEmail.Margin = new Padding(3, 2, 3, 2);
             textBoxEmail.Name = "textBoxEmail";
-            textBoxEmail.Size = new Size(267, 38);
+            textBoxEmail.Size = new Size(234, 32);
             textBoxEmail.TabIndex = 109;
             // 
             // textBoxNrTelefonu
             // 
             textBoxNrTelefonu.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxNrTelefonu.Location = new Point(190, 551);
+            textBoxNrTelefonu.Location = new Point(166, 413);
+            textBoxNrTelefonu.Margin = new Padding(3, 2, 3, 2);
             textBoxNrTelefonu.Name = "textBoxNrTelefonu";
-            textBoxNrTelefonu.Size = new Size(267, 38);
+            textBoxNrTelefonu.Size = new Size(234, 32);
             textBoxNrTelefonu.TabIndex = 108;
             // 
             // textBoxNazwisko
             // 
             textBoxNazwisko.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxNazwisko.Location = new Point(190, 174);
+            textBoxNazwisko.Location = new Point(166, 130);
+            textBoxNazwisko.Margin = new Padding(3, 2, 3, 2);
             textBoxNazwisko.Name = "textBoxNazwisko";
-            textBoxNazwisko.Size = new Size(267, 38);
+            textBoxNazwisko.Size = new Size(234, 32);
             textBoxNazwisko.TabIndex = 103;
             // 
             // textBoxLogin
             // 
             textBoxLogin.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxLogin.Location = new Point(190, 238);
+            textBoxLogin.Location = new Point(166, 178);
+            textBoxLogin.Margin = new Padding(3, 2, 3, 2);
             textBoxLogin.Name = "textBoxLogin";
-            textBoxLogin.Size = new Size(267, 38);
+            textBoxLogin.Size = new Size(234, 32);
             textBoxLogin.TabIndex = 102;
             // 
             // textBoxHaslo
             // 
             textBoxHaslo.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxHaslo.Location = new Point(190, 309);
+            textBoxHaslo.Location = new Point(166, 232);
+            textBoxHaslo.Margin = new Padding(3, 2, 3, 2);
             textBoxHaslo.Name = "textBoxHaslo";
-            textBoxHaslo.Size = new Size(267, 38);
+            textBoxHaslo.Size = new Size(234, 32);
             textBoxHaslo.TabIndex = 101;
             // 
             // textBoxKodPocztowy
             // 
             textBoxKodPocztowy.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxKodPocztowy.Location = new Point(666, 109);
+            textBoxKodPocztowy.Location = new Point(583, 82);
+            textBoxKodPocztowy.Margin = new Padding(3, 2, 3, 2);
             textBoxKodPocztowy.Name = "textBoxKodPocztowy";
-            textBoxKodPocztowy.Size = new Size(267, 38);
+            textBoxKodPocztowy.Size = new Size(234, 32);
             textBoxKodPocztowy.TabIndex = 100;
             // 
             // textBoxUlica
             // 
             textBoxUlica.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxUlica.Location = new Point(666, 168);
+            textBoxUlica.Location = new Point(583, 126);
+            textBoxUlica.Margin = new Padding(3, 2, 3, 2);
             textBoxUlica.Name = "textBoxUlica";
-            textBoxUlica.Size = new Size(267, 38);
+            textBoxUlica.Size = new Size(234, 32);
             textBoxUlica.TabIndex = 99;
             // 
             // textBoxNrPosesji
             // 
             textBoxNrPosesji.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxNrPosesji.Location = new Point(666, 232);
+            textBoxNrPosesji.Location = new Point(583, 174);
+            textBoxNrPosesji.Margin = new Padding(3, 2, 3, 2);
             textBoxNrPosesji.Name = "textBoxNrPosesji";
-            textBoxNrPosesji.Size = new Size(267, 38);
+            textBoxNrPosesji.Size = new Size(234, 32);
             textBoxNrPosesji.TabIndex = 98;
             // 
             // textBoxNrLokalu
             // 
             textBoxNrLokalu.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxNrLokalu.Location = new Point(662, 306);
+            textBoxNrLokalu.Location = new Point(579, 230);
+            textBoxNrLokalu.Margin = new Padding(3, 2, 3, 2);
             textBoxNrLokalu.Name = "textBoxNrLokalu";
-            textBoxNrLokalu.Size = new Size(267, 38);
+            textBoxNrLokalu.Size = new Size(234, 32);
             textBoxNrLokalu.TabIndex = 97;
             // 
             // textBoxPesel
             // 
             textBoxPesel.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxPesel.Location = new Point(190, 370);
+            textBoxPesel.Location = new Point(166, 278);
+            textBoxPesel.Margin = new Padding(3, 2, 3, 2);
             textBoxPesel.Name = "textBoxPesel";
-            textBoxPesel.Size = new Size(267, 38);
+            textBoxPesel.Size = new Size(234, 32);
             textBoxPesel.TabIndex = 96;
             // 
             // label18
             // 
             label18.AutoSize = true;
             label18.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label18.Location = new Point(47, 184);
+            label18.Location = new Point(41, 138);
             label18.Name = "label18";
-            label18.Size = new Size(123, 28);
+            label18.Size = new Size(102, 23);
             label18.TabIndex = 95;
             label18.Text = "Nazwisko:";
             // 
@@ -208,9 +222,9 @@
             // 
             label17.AutoSize = true;
             label17.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label17.Location = new Point(91, 248);
+            label17.Location = new Point(80, 186);
             label17.Name = "label17";
-            label17.Size = new Size(78, 28);
+            label17.Size = new Size(65, 23);
             label17.TabIndex = 94;
             label17.Text = "Login:";
             // 
@@ -218,9 +232,9 @@
             // 
             label16.AutoSize = true;
             label16.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label16.Location = new Point(90, 319);
+            label16.Location = new Point(79, 239);
             label16.Name = "label16";
-            label16.Size = new Size(79, 28);
+            label16.Size = new Size(65, 23);
             label16.TabIndex = 93;
             label16.Text = "Hasło:";
             // 
@@ -228,9 +242,9 @@
             // 
             label15.AutoSize = true;
             label15.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label15.Location = new Point(474, 118);
+            label15.Location = new Point(415, 88);
             label15.Name = "label15";
-            label15.Size = new Size(170, 28);
+            label15.Size = new Size(138, 23);
             label15.TabIndex = 92;
             label15.Text = "Kod pocztowy:";
             // 
@@ -238,9 +252,9 @@
             // 
             label14.AutoSize = true;
             label14.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label14.Location = new Point(572, 180);
+            label14.Location = new Point(500, 135);
             label14.Name = "label14";
-            label14.Size = new Size(72, 28);
+            label14.Size = new Size(60, 23);
             label14.TabIndex = 91;
             label14.Text = "Ulica:";
             // 
@@ -248,9 +262,9 @@
             // 
             label13.AutoSize = true;
             label13.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label13.Location = new Point(522, 238);
+            label13.Location = new Point(457, 178);
             label13.Name = "label13";
-            label13.Size = new Size(122, 28);
+            label13.Size = new Size(100, 23);
             label13.TabIndex = 90;
             label13.Text = "Nr posesji:";
             // 
@@ -258,9 +272,9 @@
             // 
             label12.AutoSize = true;
             label12.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label12.Location = new Point(526, 315);
+            label12.Location = new Point(460, 236);
             label12.Name = "label12";
-            label12.Size = new Size(118, 28);
+            label12.Size = new Size(98, 23);
             label12.TabIndex = 89;
             label12.Text = "Nr lokalu:";
             // 
@@ -268,9 +282,9 @@
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label11.Location = new Point(4, 380);
+            label11.Location = new Point(4, 285);
             label11.Name = "label11";
-            label11.Size = new Size(165, 28);
+            label11.Size = new Size(138, 23);
             label11.TabIndex = 88;
             label11.Text = "Numer PESEL:";
             // 
@@ -278,9 +292,9 @@
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label10.Location = new Point(5, 430);
+            label10.Location = new Point(4, 322);
             label10.Name = "label10";
-            label10.Size = new Size(179, 28);
+            label10.Size = new Size(148, 23);
             label10.TabIndex = 87;
             label10.Text = "Data urodzenia:";
             // 
@@ -288,9 +302,9 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label9.Location = new Point(91, 490);
+            label9.Location = new Point(80, 368);
             label9.Name = "label9";
-            label9.Size = new Size(78, 28);
+            label9.Size = new Size(66, 23);
             label9.TabIndex = 86;
             label9.Text = "Email:";
             // 
@@ -298,36 +312,38 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(32, 561);
+            label4.Location = new Point(28, 421);
             label4.Name = "label4";
-            label4.Size = new Size(137, 28);
+            label4.Size = new Size(112, 23);
             label4.TabIndex = 81;
             label4.Text = "Nr telefonu:";
             // 
             // textBoxImie
             // 
             textBoxImie.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxImie.Location = new Point(190, 112);
+            textBoxImie.Location = new Point(166, 84);
+            textBoxImie.Margin = new Padding(3, 2, 3, 2);
             textBoxImie.Name = "textBoxImie";
-            textBoxImie.Size = new Size(267, 38);
+            textBoxImie.Size = new Size(234, 32);
             textBoxImie.TabIndex = 80;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(105, 122);
+            label3.Location = new Point(92, 92);
             label3.Name = "label3";
-            label3.Size = new Size(64, 28);
+            label3.Size = new Size(53, 23);
             label3.TabIndex = 79;
             label3.Text = "Imie:";
             // 
             // button2
             // 
             button2.Font = new Font("Book Antiqua", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(54, -78);
+            button2.Location = new Point(47, -58);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(164, 54);
+            button2.Size = new Size(144, 40);
             button2.TabIndex = 78;
             button2.Text = "Zaloguj";
             button2.UseVisualStyleBackColor = true;
@@ -336,9 +352,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Book Antiqua", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(54, -149);
+            label2.Location = new Point(47, -112);
             label2.Name = "label2";
-            label2.Size = new Size(268, 56);
+            label2.Size = new Size(218, 46);
             label2.TabIndex = 77;
             label2.Text = "Jeśli posiadasz już konto\r\n zaloguj się";
             // 
@@ -346,9 +362,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Book Antiqua", 28.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(518, 23);
+            label1.Location = new Point(453, 17);
             label1.Name = "label1";
-            label1.Size = new Size(411, 58);
+            label1.Size = new Size(331, 45);
             label1.TabIndex = 76;
             label1.Text = "Dodaj pracownika";
             // 
@@ -356,19 +372,21 @@
             // 
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(474, 380);
+            dataGridView1.Location = new Point(415, 285);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(848, 369);
+            dataGridView1.Size = new Size(742, 277);
             dataGridView1.TabIndex = 114;
             // 
             // ButtonOdswiez
             // 
             ButtonOdswiez.Font = new Font("Book Antiqua", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            ButtonOdswiez.Location = new Point(190, 687);
+            ButtonOdswiez.Location = new Point(166, 515);
+            ButtonOdswiez.Margin = new Padding(3, 2, 3, 2);
             ButtonOdswiez.Name = "ButtonOdswiez";
-            ButtonOdswiez.Size = new Size(169, 62);
+            ButtonOdswiez.Size = new Size(148, 46);
             ButtonOdswiez.TabIndex = 115;
             ButtonOdswiez.Text = "Odśwież";
             ButtonOdswiez.UseVisualStyleBackColor = true;
@@ -376,10 +394,11 @@
             // 
             // DodajPracownikaForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.stan_czytelnictwa_w_polsce1;
-            ClientSize = new Size(1382, 803);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1209, 602);
             Controls.Add(ButtonOdswiez);
             Controls.Add(dataGridView1);
             Controls.Add(dateTimePicker1);
@@ -413,6 +432,7 @@
             Controls.Add(label1);
             Controls.Add(buttonRejstracja);
             Controls.Add(buttonZalogujRej);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "DodajPracownikaForm";
             Text = "DodajPracownikaForm";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
