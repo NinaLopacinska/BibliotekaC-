b43734b519ac2db7bc3b99e41bfa0ed29c376b11
Nina Łopacińska <UL0273299@edu.uni.lodz.pl>
2024-11-23 19:46:05 +0100

    edycja worda

--- bilbioteka/Forms/StatystykiPracownik.Designer.cs
+++ bilbioteka/Forms/StatystykiPracownik.Designer.cs
@@ -47,10 +47,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(216, 49);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(173, 39);
             label2.Name = "label2";
-            label2.Size = new Size(390, 47);
+            label2.Size = new Size(334, 40);
             label2.TabIndex = 8;
             label2.Text = "Ilość użytkowników:";
             // 
@@ -58,10 +57,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(216, 138);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(173, 110);
             label1.Name = "label1";
-            label1.Size = new Size(574, 47);
+            label1.Size = new Size(492, 40);
             label1.TabIndex = 9;
             label1.Text = "Ilość dokonanych wypożyczeń:";
             // 
@@ -69,10 +67,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(216, 326);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(173, 261);
             label3.Name = "label3";
-            label3.Size = new Size(497, 47);
+            label3.Size = new Size(423, 40);
             label3.TabIndex = 10;
             label3.Text = "Najpopularniejsze pozycje:";
             // 
@@ -80,10 +77,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(216, 225);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(173, 180);
             label4.Name = "label4";
-            label4.Size = new Size(727, 47);
+            label4.Size = new Size(621, 40);
             label4.TabIndex = 11;
             label4.Text = "Najczęściej wypożyczający użytkownik:";
             // 
@@ -91,21 +87,19 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(216, 673);
-            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Location = new Point(173, 538);
             label5.Name = "label5";
-            label5.Size = new Size(631, 47);
+            label5.Size = new Size(417, 40);
             label5.TabIndex = 12;
-            label5.Text = "Najpopularniejsze gatunki książek";
+            label5.Text = "Najpopularniejsze gatunki\r\n";
             // 
             // labelIloscUzytkownikow
             // 
             labelIloscUzytkownikow.AutoSize = true;
             labelIloscUzytkownikow.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            labelIloscUzytkownikow.Location = new Point(646, 49);
-            labelIloscUzytkownikow.Margin = new Padding(4, 0, 4, 0);
+            labelIloscUzytkownikow.Location = new Point(517, 39);
             labelIloscUzytkownikow.Name = "labelIloscUzytkownikow";
-            labelIloscUzytkownikow.Size = new Size(390, 47);
+            labelIloscUzytkownikow.Size = new Size(334, 40);
             labelIloscUzytkownikow.TabIndex = 13;
             labelIloscUzytkownikow.Text = "Ilość użytkowników:";
             // 
@@ -113,10 +107,9 @@
             // 
             labelIloscWypozyczen.AutoSize = true;
             labelIloscWypozyczen.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            labelIloscWypozyczen.Location = new Point(821, 138);
-            labelIloscWypozyczen.Margin = new Padding(4, 0, 4, 0);
+            labelIloscWypozyczen.Location = new Point(657, 110);
             labelIloscWypozyczen.Name = "labelIloscWypozyczen";
-            labelIloscWypozyczen.Size = new Size(390, 47);
+            labelIloscWypozyczen.Size = new Size(334, 40);
             labelIloscWypozyczen.TabIndex = 14;
             labelIloscWypozyczen.Text = "Ilość użytkowników:";
             // 
@@ -124,40 +117,41 @@
             // 
             labelUzytkownik.AutoSize = true;
             labelUzytkownik.Font = new Font("Book Antiqua", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            labelUzytkownik.Location = new Point(972, 225);
-            labelUzytkownik.Margin = new Padding(4, 0, 4, 0);
+            labelUzytkownik.Location = new Point(778, 180);
             labelUzytkownik.Name = "labelUzytkownik";
-            labelUzytkownik.Size = new Size(390, 47);
+            labelUzytkownik.Size = new Size(334, 40);
             labelUzytkownik.TabIndex = 15;
             labelUzytkownik.Text = "Ilość użytkowników:";
             // 
             // dataGridViewPozycja
             // 
             dataGridViewPozycja.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewPozycja.Location = new Point(216, 407);
+            dataGridViewPozycja.Location = new Point(173, 326);
+            dataGridViewPozycja.Margin = new Padding(2, 2, 2, 2);
             dataGridViewPozycja.Name = "dataGridViewPozycja";
             dataGridViewPozycja.RowHeadersWidth = 62;
             dataGridViewPozycja.RowTemplate.Height = 33;
-            dataGridViewPozycja.Size = new Size(995, 225);
+            dataGridViewPozycja.Size = new Size(796, 180);
             dataGridViewPozycja.TabIndex = 16;
             // 
             // dataGridViewGatunki
             // 
             dataGridViewGatunki.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewGatunki.Location = new Point(216, 745);
+            dataGridViewGatunki.Location = new Point(173, 596);
+            dataGridViewGatunki.Margin = new Padding(2, 2, 2, 2);
             dataGridViewGatunki.Name = "dataGridViewGatunki";
             dataGridViewGatunki.RowHeadersWidth = 62;
             dataGridViewGatunki.RowTemplate.Height = 33;
-            dataGridViewGatunki.Size = new Size(995, 225);
+            dataGridViewGatunki.Size = new Size(796, 180);
             dataGridViewGatunki.TabIndex = 17;
             // 
             // buttonZalogujRej
             // 
             buttonZalogujRej.Font = new Font("Book Antiqua", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonZalogujRej.Location = new Point(13, 19);
-            buttonZalogujRej.Margin = new Padding(4, 3, 4, 3);
+            buttonZalogujRej.Location = new Point(10, 15);
+            buttonZalogujRej.Margin = new Padding(3, 2, 3, 2);
             buttonZalogujRej.Name = "buttonZalogujRej";
-            buttonZalogujRej.Size = new Size(110, 93);
+            buttonZalogujRej.Size = new Size(88, 74);
             buttonZalogujRej.TabIndex = 166;
             buttonZalogujRej.Text = " ⬅";
             buttonZalogujRej.UseVisualStyleBackColor = true;
@@ -165,10 +159,10 @@
             // 
             // StatystykiPracownik
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.stan_czytelnictwa_w_polsce1;
-            ClientSize = new Size(1731, 1005);
+            ClientSize = new Size(1385, 804);
             Controls.Add(buttonZalogujRej);
             Controls.Add(dataGridViewGatunki);
             Controls.Add(dataGridViewPozycja);
@@ -180,6 +174,7 @@
             Controls.Add(label3);
             Controls.Add(label1);
             Controls.Add(label2);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "StatystykiPracownik";
             Text = "StatystykiPracownik";
             ((System.ComponentModel.ISupportInitialize)dataGridViewPozycja).EndInit();
